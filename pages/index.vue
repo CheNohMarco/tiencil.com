f77a70a04aeefa2b9f5a6d3983dbfab35c955597
CheNohMarco <marcoantoniochenoh@gmail.com>
2023-02-11 16:04:52 -0500

    primer avance para landing page de tiencil

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <NavBar/>
+    <Services/>
+    <About-us/>
+    <Contact/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import AboutUs from '../components/tiencil/AboutUs.vue';
+import Contact from '../components/tiencil/Contact.vue';
+import Footer from '../components/tiencil/Footer.vue';
+import NavBar from '../components/tiencil/NavBar.vue';
+import Services from '../components/tiencil/Services-t.vue';
+
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { NavBar, Services, AboutUs, Contact, Footer }
 }
 </script>
